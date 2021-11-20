ab8668223bff95d81233ac3f76897cac6b280c87
unknown <arbi.nmaki919@gmail.com>
2021-11-20 17:45:17 +0700

    deploy

---
+++ app/users/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { viewSignIn, actionSignIn, actionLogout } = require('./controller');
+
+router.get('/', viewSignIn);
+router.post('/', actionSignIn);
+router.get('/logout', actionLogout);
+
+module.exports = router;
